6bf82526da263d6e150ab2ec2e59f8079654717f
SaintMortal <saintmortal1337@gmail.com>
2026-02-20 00:45:45 +0100

    start

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "Hello, World!";
+    int length = ft_strlen(str);
+    printf("Length of the string: %d\n", length);
+    return 0;
+}
