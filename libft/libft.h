2453aaf258328404b8cfb4dd5c5fb7484db4542e
SaintMortal <saintmortal1337@gmail.com>
2026-02-21 22:31:40 +0100

    changes

--- libft/libft.h
+++ libft/libft.h
@@ -4,7 +4,11 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-int ft_strlen(char *str);
+int ft_strlen(const char *str);
 void ft_putnbr(int n);
+int	ft_atoi(char *str);
+char *ft_strdup(const char *s1);
+char *ft_strcpy(char *dst, const char *src);
+
 
 #endif
