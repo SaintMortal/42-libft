f189043c4cbc127263382cd0c63607c737578893
SaintMortal <saintmortal1337@gmail.com>
2026-03-10 19:15:35 +0100

    memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,13 @@
+void *ft_memset(void *b, int c, size_t len)
+{
+    unsigned char *ptr;
+
+    ptr = (unsigned char *)b;
+    while (len > 0)
+    {
+        *ptr = (unsigned char)c;
+        ptr++;
+        len--;
+    }
+    return (b);
+}
