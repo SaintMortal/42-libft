e07d28acf9e929e663899ec9e951322f96720613
SaintMortal <saintmortal1337@gmail.com>
2026-03-22 23:12:50 +0100

    memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *p1;
+    const unsigned char *p2;
+    size_t i;
+
+    p1 = (const unsigned char *)s1;
+    p2 = (const unsigned char *)s2;
+    while (i < n)
+    {
+        if(p1[i] != p2[i])
+            return (p1[i] - p2[i]);
+        i++;
+    }
+    return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -3,6 +3,7 @@
 
 #include<stdlib.h>
 #include<unistd.h>
+#include<stddef.h>
 
 int ft_strlen(const char *str);
 void ft_putnbr(int n);
@@ -22,4 +23,6 @@ void *ft_memset(void *b, int c, size_t len);
 void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
