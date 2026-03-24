47ba400d6ec0596232b15930f3e5153a35cca1f2
SaintMortal <saintmortal1337@gmail.com>
2026-03-24 23:29:22 +0100

    ft_strlcat

--- libft/libft.h
+++ libft/libft.h
@@ -26,4 +26,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
 #endif
