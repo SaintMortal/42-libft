4ac640be88eaca0c421de378f99fcee4ce5b7c27
SaintMortal <saintmortal1337@gmail.com>
2026-04-03 01:07:07 +0200

    strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t i;
+    size_t j;
+
+    if (*needle == '\0')
+        return ((char *)haystack);
+    if (len == 0)
+        return (NULL);
+    i = 0;
+    j = 0;
+    while (haystack[i] != '\0' && i < len)
+    {
+            while (haystack[i + j] == needle[j] && (i + j) < len && needle[j] != '\0')
+            {
+                j++;
+            }
+            if (needle[j] == '\0')
+                return ((char *)haystack + i);
+            i++;
+    }
+    return (NULL);
+}
