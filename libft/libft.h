f87523cd7869cedb5c49441e88894afd72c62440
SaintMortal <saintmortal1337@gmail.com>
2026-04-03 01:14:18 +0200

    isalnum

--- libft/libft.h
+++ libft/libft.h
@@ -32,4 +32,5 @@ char *ft_strrchr(const char *s, int c);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 int ft_isalpha(int c);
 int ft_isdigit(int c);
+int ft_isalnum(int c);
 #endif
