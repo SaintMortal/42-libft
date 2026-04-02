6c557f590cb711b1bd5093e2633758286ad87422
SaintMortal <saintmortal1337@gmail.com>
2026-04-03 01:29:42 +0200

    calloc

--- libft/libft.h
+++ libft/libft.h
@@ -37,4 +37,6 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+void *ft_calloc(size_t count, size_t size);
 #endif
